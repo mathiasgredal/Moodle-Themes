ea2009f935d0e1e7f6c18c37cedc5986728b37d9
mathiasgredal <mathiasgredal@icloud.com>
2018-09-25 13:52:10 +0200

    Update Tampermonkey-darkmode.js

--- Tampermonkey-darkmode.js
+++ Tampermonkey-darkmode.js
@@ -8,27 +8,12 @@
 // @grant        none
 // ==/UserScript==
 
-function css(el) {
-    var sheets = document.styleSheets, ret = [];
-    el.matches = el.matches || el.webkitMatchesSelector || el.mozMatchesSelector
-        || el.msMatchesSelector || el.oMatchesSelector;
-    for (var i in sheets) {
-        var rules = sheets[i].rules || sheets[i].cssRules;
-        for (var r in rules) {
-            if (el.matches(rules[r].selectorText)) {
-                ret.push(rules[r].cssText);
-            }
-        }
-    }
-    return ret;
-}
-
 (function() {
     'use strict';
 
     // Your code here...
 
-    var background = "#2C2F33"
+    var background = "#fff"
     var card = "#23272A"
     var header = "#23272A"
     var button = "#23272A"
