1631d5d58be288a50fd510fb2747373c3594414c
mathiasgredal <mathiasgredal@icloud.com>
2018-09-25 17:16:24 +0200

    Create tampermonkey.user.js

---
+++ tampermonkey.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Moodle 2.0
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://moodle.zbc.dk/*
+// @grant        none
+// @require      https://github.com/mathiasgredal/Moodle-Themes/raw/master/Tampermonkey-darkmode.js
+// @updateURL    https://github.com/mathiasgredal/Moodle-Themes/raw/master/Tampermonkey-darkmode.js
+// @downloadURL  https://github.com/mathiasgredal/Moodle-Themes/raw/master/Tampermonkey-darkmode.js
+// ==/UserScript==
+
+
+init();
+
