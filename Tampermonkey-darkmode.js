e1bf247cd3712496dbed8c8f2e0be1cb838eeb94
mathiasgredal <mathiasgredal@icloud.com>
2018-09-25 17:15:31 +0200

    Update and rename Tampermonkey-darkmode.user.js to Tampermonkey-darkmode.js

--- Tampermonkey-darkmode.js
+++ Tampermonkey-darkmode.js
@@ -1,15 +1,3 @@
-// ==UserScript==
-// @name         Moodle 2.0
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://moodle.zbc.dk/*
-// @grant        none
-// @updateURL   https://github.com/mathiasgredal/Moodle-Themes/raw/master/Tampermonkey-darkmode.user.js
-// @downloadURL https://github.com/mathiasgredal/Moodle-Themes/raw/master/Tampermonkey-darkmode.user.js
-// ==/UserScript==
-
 function init() {
     'use strict';
 
